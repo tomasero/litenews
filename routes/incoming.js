d0a4b321eee222bd6210fc7eab93f845950aa4f0
Tomas Vega <tomasvegagalvez@gmail.com>
2014-08-21 19:29:48 -0700

    deleted toSMS method to test routing first

--- routes/incoming.js
+++ routes/incoming.js
@@ -1,62 +1,64 @@
+var express = require('express');
+var router = express.Router();
+
 var newsArray = null;
 var currentNews = null;
 var newsCounter = 0;
 var sentenceCounter = 0;
 
-app.post('/incoming', function(req, res) {
+router.get('/incoming', function(req, res) {
     var keyword = req.body.Body.toLowerCase();
     if (keyword == 'more') {
         if (newsArray != null) {
             if (currentNews != null && sentenceCounter < currentNews.sentences.length) {
-                var response = currentNews.sentences[sentenceCounter];//substring(0, 160); //160 is limit for string
+                var response = '<Response><Sms>' + currentNews.sentences[sentenceCounter] + '</Sms></Response>'; //.substring(0, 160)
                 sentenceCounter++;
-                res.send(toSMS(response));
+                res.send(response);
             } else {
                 sentenceCounter = 0;
-                var response = 'Try writing \'next\' or a new topic';
-                res.send(toSMS(response));
+                var response = '<Response><Sms>Try writing \'next\' or a new topic</Sms></Response>';
+                res.send(response);
             }
         } else {
-            var response = 'No current news, do a new search!';
-            res.send(toSMS(response));
+            var response = '<Response><Sms>No current news, do a new search!</Sms></Response>';
+            res.send(response);
         }
     } else if (keyword == 'tech' || keyword == 'world' | keyword == 'business') {
         var url = 'http://bitofnews.com/api/'+keyword+'/';
         restler.get(url, { parser: restler.parsers.json }).on('complete', function(news) {
             newsArray = news;
             currentNews = news[newsCounter];
+            var response = '<Response><Sms>' + currentNews.title + '</Sms></Response>';
             newsCounter++;
-            res.send(toSMS(currentNews.title));
+            res.send(response);
         });
     } else if (keyword == 'next') {
         sentenceCounter = 0;
         if (newsArray != null) {
             if (newsCounter < newsArray.length) {
                 currentNews = newsArray[newsCounter];
+                var response = '<Response><Sms>' + currentNews.title + '</Sms></Response>';
                 newsCounter++;
-                res.send(toSMS(currentNews.title));
+                res.send(response);
             } else {
                 newsArray = null;
                 currentNews = null;
                 newsCounter = 0;
                 sentenceCounter = 0;
-                var response = 'No more news, try a new topic';
-                res.send(toSMS(response));
+                var response = '<Response><Sms>No more news, try a new topic</Sms></Response>';
+                res.send(response);
             }
         } else {
             var response = '<Response><Sms>No current news, do a new search!</Sms></Response>';
             res.send(response);
         }
     } else {
-        var response = 'We can\'t find news about that topic';
-        res.send(toSMS(response));
+        var response = '<Response><Sms>We can\'t find news about that topic</Sms></Response>';
+        res.send(response);
     }
 });
 
-function toSMS(something) {
-    var response = '<Response><Sms>' + something + '</Sms></Response>';
-    return response;
-}
+module.exports = router;
 
 /**
 // Load the twilio module
