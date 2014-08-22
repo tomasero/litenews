e5f137a3a1ff7b88aa289de68fa1574351aa2e61
Tomas Vega <tomasvegagalvez@gmail.com>
2014-08-21 23:12:34 -0700

    fixed range of conditional

--- app.js
+++ app.js
@@ -42,8 +42,8 @@ app.post('/incoming', function(req, res) {
     var keyword = req.body.Body.toLowerCase();
     
     if (!isNaN(keyword)) {
-        var index = Number(keyword);
-        if (index < newsArray.length) {
+        var index = Number(keyword) + 1;
+        if (index > 0 && index <= newsArray.length) {
             currentNews = newsArray[index];
             sentenceCounter = 0;
             var response = '<Response><Sms>' + currentNews.title + '</Sms></Response>';
