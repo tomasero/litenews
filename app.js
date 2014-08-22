9ddf9687c1b3d36e2f03dccf75f52ca8fd07fcc1
Tomas Vega <tomasvegagalvez@gmail.com>
2014-08-21 18:50:26 -0700

    debuggin more

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.post('/incoming', function(req, res) {
                 var response = '<Response><Sms>' + currentNews.sentences[sentenceCounter] + '</Sms></Response>';
                 res.send(response);
             } else {
-                res.send('<Response><Sms>' + currentNews.length + '</Sms></Response>');
+                res.send('<Response><Sms>' + currentNews + '</Sms></Response>');
                 sentenceCounter = 0;
                 var response = '<Response><Sms>Try writing \'next\' or a new topic</Sms></Response>';
                 res.send(response);
