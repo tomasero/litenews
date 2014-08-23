ccc951f25944a5398be285614f395e20aa39abeb
Tomas Vega <tomasvegagalvez@gmail.com>
2014-08-22 19:38:50 -0700

    nope not working

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ var moreInfo = false;
 
 app.post('/incoming', function(req, res) {
     var keyword = req.body.Body.toLowerCase();
+    res.send(toResponse(toSMS(keyword)));
+    /**
     if (!isNaN(keyword)) {
         var response = '';
         if (newsArray != null) {
@@ -97,6 +99,7 @@ app.post('/incoming', function(req, res) {
         var response = toSMS('We can\'t find news about that topic');
         res.send(toResponse(response));
     }
+    **/
 });
 
 function getHeadlines(array) {
